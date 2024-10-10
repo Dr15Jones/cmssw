aea4bdc116c02b4d0bf6858a18b10fe15447ca82
Christopher Jones <chrisdjones15@gmail.com>
2024-10-10 08:13:40 -0500

    Add additional memory parameters to RNTupleOutputModule

--- IOPool/RNTupleOutput/plugins/RNTupleOutputFile.cc
+++ IOPool/RNTupleOutput/plugins/RNTupleOutputFile.cc
@@ -189,9 +189,9 @@ namespace edm {
       writeOptions.SetCompression(convert(iConfig.compressionAlgo), iConfig.compressionLevel);
       writeOptions.SetApproxZippedClusterSize(iConfig.approxZippedClusterSize);
       writeOptions.SetMaxUnzippedClusterSize(iConfig.maxUnzippedClusterSize);
-      //writeOptions.SetInitialNElementsPerPage(iConfig.initialNElementsPerPage);
-      //writeOptions.SetMaxUnzippedPageSize(iConig.maxUnzippedPageSize);
-      //writeOptions.SetPageBufferBudget(iConfig.pageBufferBudget);
+      writeOptions.SetInitialNElementsPerPage(iConfig.initialNElementsPerPage);
+      writeOptions.SetMaxUnzippedPageSize(iConfig.maxUnzippedPageSize);
+      writeOptions.SetPageBufferBudget(iConfig.pageBufferBudget);
       writeOptions.SetUseBufferedWrite(iConfig.useBufferedWrite);
       writeOptions.SetUseDirectIO(iConfig.useDirectIO);
       events_ = ROOT::Experimental::RNTupleWriter::Append(std::move(model), "Events", file_, writeOptions);
